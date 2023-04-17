87dec8a442250dfab89b967a298ce91fa6761b4f
Emily Libman <elibman@umich.edu>
2023-04-17 16:55:38 -0400

    api

--- emily_api.py
+++ emily_api.py
@@ -5,11 +5,29 @@ import sqlite3
 import unittest 
 import json
 
-url = "https://imdb-top-100-movies.p.rapidapi.com/"
+# url = 'https://itunes.apple.com/us/rss/topmovies/limit=100/json' # iTunes API URL to fetch top 100 movies
+
+# response = requests.get(url) # send GET request to fetch data
+# data = response.json() # parse JSON data
+
+# dct ={}
+# #print(data)
+# #iterate over the movie entries in the response and print the title and rating
+# for entry in data['feed']['entry']:
+#     #print(entry)
+#     title = entry['title']['label']
+#     rating = entry['im:rating']['label']
+#     #rating = entry['attributes']['im:id']
+#     print(rating)
+#     #print(title)
+
+import requests
+
+url = "https://mdblist.p.rapidapi.com/"
 
 headers = {
 	"X-RapidAPI-Key": "536c152bbemshbc7cbb92a43fe41p1ff4fbjsnba9018e95bd7",
-	"X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com"
+	"X-RapidAPI-Host": "mdblist.p.rapidapi.com"
 }
 
 response = requests.request("GET", url, headers=headers)
