2574908ecb58e760fbaa4c94f9fd610dbd7fb179
Louis Ciccone <cicconel@umich.edu>
2023-04-17 22:16:04 -0400

    m

--- louis.py
+++ louis.py
@@ -1,35 +1,43 @@
 import requests
-import matplotlib.pyplot as plt
+import json
+import unittest
 import os
 import sqlite3
-import unittest 
-import json
 
-# url = 'https://itunes.apple.com/us/rss/topmovies/limit=100/json' # iTunes API URL to fetch top 100 movies
 
-# response = requests.get(url) # send GET request to fetch data
-# data = response.json() # parse JSON data
 
-# dct ={}
-# #print(data)
-# #iterate over the movie entries in the response and print the title and rating
-# for entry in data['feed']['entry']:
-#     #print(entry)
-#     title = entry['title']['label']
-#     rating = entry['im:rating']['label']
-#     #rating = entry['attributes']['im:id']
-#     print(rating)
-#     #print(title)
+def Getratingfromimdb():
 
-import requests
+    url = "https://imdb-top-100-movies.p.rapidapi.com/"
 
-url = "https://mdblist.p.rapidapi.com/"
+    headers = {
+	"X-RapidAPI-Key": "d53ac055e7mshea37be8e69920fap17a024jsnc6076add2e01",
+	"X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com"
+    }
 
-headers = {
-	"X-RapidAPI-Key": "536c152bbemshbc7cbb92a43fe41p1ff4fbjsnba9018e95bd7",
-	"X-RapidAPI-Host": "mdblist.p.rapidapi.com"
-}
+    response = requests.request("GET", url, headers=headers)
+    data = response.text
+    d = json.loads(data)
 
-response = requests.request("GET", url, headers=headers)
+    info = {}
+    for x in d:
+        info[x["title"]] = x["rating"]
+    
+    with sqlite3.connect('imdb.db') as conn:
+        cur = conn.cursor()
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS imdb (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                rating INTEGER NOT NULL
+            );
+        ''')
+        conn.commit()
 
-print(response.text)
+        for title, rating in info.items():
+            cur.execute('INSERT INTO imdb (title, rating) VALUES (?, ?)',
+            (title, rating))
+            conn.commit()
+        cur.close()
+
+Getratingfromimdb()
